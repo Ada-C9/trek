3a657d25b477ecc5aa3cba18b06a807fd6cdcaf3
emilcecarlisa <emilcecarlisa@gmail.com>
2018-05-22 15:40:50 -0700

    Initial setup complete

---
+++ index.js
@@ -0,0 +1,12 @@
+const allTripsUrl = " https://ada-backtrek-api.herokuapp.com/trips"
+
+const getTrips = function getTrips() {
+  console.log("Am i inside?");
+}
+
+
+
+$(document).ready(() => {
+   // $('#load').click(loadWonders);
+   getTrips()
+});
