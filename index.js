db7200e2292e48d7597de1b2f7e5a63f00550dc6
Kiera <kiera.thomasson@gmail.com>
2018-05-22 16:14:01 -0700

    making progress on axios.get

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const URL = "https://ada-backtrek-api.herokuapp.com/trips";
+const BASEURL = "https://ada-backtrek-api.herokuapp.com/trips";
+
+
+const loadAllTrips = () => {
+  const trips = $("#trip-table");
+  trips.empty();
+
+  axios.get(BASEURL)
+    .then((response) => {
+      console.log(response);
+      let data = response.data;
+
+      data.forEach((trip) => {
+        console.log(trip.name);
+        $('#trip-table').html(trip.name);
+      });
+      // trips.html(`<tbody>${data}</tbody>`);
+    });
+};
+
+
+$(document).ready(() => {
+  console.log('YOUR IN!');
+  $('#all-trips').click(loadAllTrips);
+})
