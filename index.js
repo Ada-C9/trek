649123b670f963b89c27ef85e92a95bb10975434
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-22 15:44:42 -0700

    Set up document dot ready and status reporting

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-$(document).ready(function() {
+const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
+
+// Status Management
+const reportStatus = function reportStatus(message) {
+  $('#display-status').html(message);
+}
+
+$(document).ready(() => {
+  $('#display-trips').click(() => {
+  });
 });
