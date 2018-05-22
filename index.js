92efb799045a4a9bf7f1e6912bf21401206eeca4
emilcecarlisa <emilcecarlisa@gmail.com>
2018-05-22 16:07:12 -0700

    Displaying all trips

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const allTripsUrl = " https://ada-backtrek-api.herokuapp.com/trips"
 
-const getTrips = function getTrips() {
-  console.log("Am i inside?");
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+};
+
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p>`;
+  content += '<ul>';
 }
 
 
+// axios.get(allTripsUrl)
+const getTrips = function getTrips() {
+  // console.log("Am i inside?");
+  let tripList = $('.trips')
+
+  axios.get(allTripsUrl)
+  .then((response) => {
+    console.log('inside the .then');
+    response.data.forEach((trip) => {
+      console.log(trip);
+      tripList.append(`<li>${trip.name}</li>`);
+    });
+    reportStatus('Trips Loaded!');
+  })
+};
 
 $(document).ready(() => {
-   // $('#load').click(loadWonders);
-   getTrips()
+  // $('#load').click(loadWonders);
+  getTrips()
 });
