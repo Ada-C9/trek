47929bf103ef694e89a706063681c6957e80148e
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-05-22 16:32:53 -0700

    Showing all trips

---
+++ index.js
@@ -0,0 +1,43 @@
+const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
+
+//
+// Status Management
+//
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+};
+
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
+const loadTrips = () => {
+  reportStatus('Loading trips...');
+
+  const tripList = $('#trip-list');
+  tripList.empty();
+
+  axios.get(URL)
+    .then((response) => {
+      reportStatus(`Successfully loaded ${response.data.length} trips`);
+      tripList.append(`<table><tr><th>All Trips</th></tr></table>`);
+      response.data.forEach((trip) => {
+        tripList.append(`<tr><td>${trip.name}</td></tr>`);
+      });
+    })
+    .catch((error) => {
+      reportStatus(`Encountered an error while loading trips: ${error.message}`);
+      console.log(error);
+    });
+};
+
+$(document).ready(() => {
+  $('#load').click(loadTrips);
+});
