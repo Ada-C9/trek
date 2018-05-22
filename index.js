8ffe691feb64ada45cb5628be16f97dfc621b323
Samantha Berk <sberk10@gmail.com>
2018-05-22 15:30:19 -0700

    add const reportError that returns errors to user using reportStatus

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const reportStatus = (message) => {
   $('#status-message').html(message)
 };
 
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li> ${field}: ${problem}<li>`;
+    }
+  }
+  content += '</ul>';
+  reportStatus(content)
+};
+
 
 $(document).ready(() => {
-  
+
 });
