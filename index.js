de1d0a8c18444f3b3633f46d2b396032a5750412
Anne <annehwatson@gmail.com>
2018-05-22 15:34:41 -0700

    load all trips request

---
+++ index.js
@@ -0,0 +1,46 @@
+const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
+
+//
+// Status Management
+//
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+};
+
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
+//
+// Loading All Trips
+//
+const loadTrips = () => {
+  reportStatus('One sec! Loading trips...');
+
+  const tripList = $('#trip-list');
+  tripList.empty();
+
+  axios.get(URL)
+    .then((response) => {
+      reportStatus(`Successfully loaded ${response.data.length} trips`);
+      response.data.forEach((trip) => {
+        tripList.append(`<li>${trip.name}</li>`);
+      });
+    })
+    .catch((error) => {
+      reportStatus(`Encountered an error while loading trips: ${error.message}`);
+      console.log(error);
+    });
+ };
+
+$(document).ready(() => {
+  $('#load-all-trips').click(loadTrips);
+  //$('#pet-form').submit(createPet);
+});
