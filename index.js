cb6a679529e240879425d731d9bad9abc44e6c4d
Samantha Berk <sberk10@gmail.com>
2018-05-22 15:27:33 -0700

    add const reportStatus and const URL

---
+++ index.js
@@ -0,0 +1,11 @@
+const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
+
+// Report Status and Errors to User
+const reportStatus = (message) => {
+  $('#status-message').html(message)
+};
+
+
+$(document).ready(() => {
+  
+});
