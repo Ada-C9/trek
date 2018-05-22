67d4aa79b9680cca4447a84a7a1d46169cf69c9d
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-22 15:58:25 -0700

    Write loadTrips function and activate when button is clicked; use axios to retrieve API data; report errors appropriately.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
 
 // Status Management
-const reportStatus = function reportStatus(message) {
+const reportStatus = (message) => {
   $('#display-status').html(message);
-}
+};
+
+const loadTrips = () => {
+  reportStatus('Loading trips');
+
+  const trips = $('#all-trips');
+  trips.empty();
+
+  axios.get(URL)
+  .then((response) => {
+    reportStatus(`Successfully loaded ${response.data.length} trips.`);
+    response.data.forEach((trip) => {
+      trips.append(`<li>${trip.name}</li>`);
+    });
+  })
+  .catch((error) => {
+    reportStatus(`There was a problem loading trips: ${error.message}`);
+    console.log(error);
+  });
+};
 
 $(document).ready(() => {
   $('#display-trips').click(() => {
+    loadTrips();
   });
 });
