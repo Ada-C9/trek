ad57a5252f43c11ec0ae0ec727275b4f2e26fe60
Phoebe <phoebe.manning8@gmail.com>
2018-05-22 18:54:31 -0700

    adds status section

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
 
+// add status in order to add colors??
+// const reportStatus = (message, status) => {}..
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+};
+
 const getTrips = () => {
   const tripList = $('#trip-list');
   tripList.empty();
+
+  reportStatus('Loading Trips...');
 
   axios.get(URL)
     .then((response) => {
       response.data.forEach((trip) =>{
         tripList.append(`<li>${trip.name}</li>`)
-        // console.log(trip.name);
       });
+      reportStatus('Trips Loaded!');
     })
     .catch((error) => {
       console.log(error.message);
+      reportStatus(`Error: ${error.message}`);
     });
 };
 
