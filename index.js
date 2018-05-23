3326e4c92a207c76c64cb44435dae76ababe8e93
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-23 11:29:01 -0700

    finished loadclickedtrip function - displaying all info for selected trip

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const loadTrips = () => {
     // console.log(response);
     tripCollection.forEach((trip) => {
       console.log(trip);
-      tripList.append(`<li class="trip ${trip.id}">${trip.name}</li>`)
+      tripList.append(`<li id="${trip.id}">${trip.name}</li>`)
     });
     userMessage(`Showing ${tripCollection.length} amazing trips to choose from!`)
   })
@@ -25,18 +25,37 @@ const loadTrips = () => {
 };
 
 function loadClickedTrip(trip) {
+   // id, name, continent, about, category, weeks and cost
   console.log(trip);
-  
+  axios.get(URL + `/${trip.id}`)
+    .then((response) => {
+      console.log(response);
+      const selectedTrip = response.data
+      $('#selected-trip').append('<h4 id="trip-table">Trip Information</h4>');
+      $('#trip-table').append(`<p>Trip Name: ${selectedTrip.name}</p>`)
+      $('#trip-table').append(`<p>Trip ID: ${selectedTrip.id}</p>`)
+      $('#trip-table').append(`<p>Continent: ${selectedTrip.continent}</p>`)
+      $('#trip-table').append(`<p>Category: ${selectedTrip.category}</p>`)
+      $('#trip-table').append(`<p>Duration: ${selectedTrip.weeks}</p>`)
+      $('#trip-table').append(`<p>Cost: $${selectedTrip.cost}</p>`)
+      $('#trip-table').append(`<p>Description: ${selectedTrip.about}</p>`)
+
+    })
+    .catch((error) => {
+      userMessage(`Hrmm.. something has gone wrong loading this trip! ${error.message}`)
+      console.log(error);
+    });
+
 }
-
-
 
 $(document).ready (() => {
   $('#trips-button').click(loadTrips);
-  $('.trip').click(function(){
-    let trip = $(this)
+  $('#trips-list').on("click", "li", function(){
+    let trip = this;
+    console.log(trip);
     loadClickedTrip(trip);
-  })
+  });
+
 });
 //   $(".note").click(function(){
 //       let note = $(this).html();
