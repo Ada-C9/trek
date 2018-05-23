4fbf8837979d0aebf8166388edd70141b5c8e24f
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-23 10:07:46 -0700

    Successfully displaying report statuses. For loading, num of trips loaded, and errors

--- trip_index.js
+++ trip_index.js
@@ -1,9 +1,11 @@
+let numTrips = 0;
+
 const loadTrips = () => {
   const URL = "https://ada-backtrek-api.herokuapp.com/trips"
-  //TODO: Reporting status needed here eventually
+  //FIXME: Reporting status supposed to be loading here. But currently not displaying trips that have loaded
+  reportStatus('Loading trip...');
 
-  // Setting up trip table that will correspond to the table with the given id in HTML
-  // And we are making sure that it empties out at the beginning
+  // Setting up trip table that will correspond to the table with the given id in HTML and it empties out at the beginning
   const tripsList = $('#trip-table');
   tripsList.empty();
 
@@ -11,18 +13,24 @@ const loadTrips = () => {
   axios.get(URL)
   // if the request is successful do the below bit
   // this should build in our table body and headers
-  // FIXME: THIS
   .then((response) => {
-    // from the response, i want the data
+    // tracking the number of trips loaded and reporting loading
+    numTrips += response.data.length;
+    reportStatus(`Successfully loaded ${numTrips} trips.`);
+    // from the response, I want the data. From Data I want trip name
     response.data.forEach((trip) => {
       tripsList.append(`<li>${trip.name}</li>`);
     });
   })
   // do this if the response is not successful
-  // TODO: Need to put in reportStatus for errors as well
   .catch((error) => {
+    reportStatus(`Encountered an error while loading trips: ${error.message}`);
     console.log(error);
   });
+};
+
+const reportStatus = (message) => {
+  $('#status-messages').html(message);
 };
 
 $(document).ready(() => {
