7888be9a53a4067ad2d9cdd28143876fb881064d
Brenda Rios <riosbrenda@gmail.com>
2018-05-23 10:42:45 -0700

    Added function to load a trip details and is working.

--- trek.js
+++ trek.js
@@ -1,4 +1,4 @@
-const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
+const URL = 'https://ada-backtrek-api.herokuapp.com/trips/';
 
 //
 // Status Management
@@ -29,9 +29,10 @@ const loadTrips = () => {
 
   axios.get(URL)
     .then((response) => {
+      console.log(response);
       reportStatus(`Successfully loaded ${response.data.length} trips`);
       response.data.forEach((trip) => {
-        tripList.append(`<li>${trip.name}</li>`);
+        tripList.append(`<li id="${trip.id}">${trip.name}</li>`);
       });
     })
     .catch((error) => {
@@ -39,6 +40,33 @@ const loadTrips = () => {
       console.log(error);
     });
 };
+
+$(`#trips-list`).on(`click`, `li`, function(){
+  console.log(this.id);
+  const tripId = this.id
+  reportStatus("Loading trip details...");
+
+  const trip = $('#trip');
+  trip.empty();
+
+  let urlTrip = URL + tripId;
+  axios.get(urlTrip)
+  .then((response) => {
+
+     reportStatus(`Successfully loaded trip to: ${response.data.name}`);
+
+     trip.append(
+       `<tr><th>${response.data.name}</th></tr>
+       <tr><th>${response.data.continent} weeks</th></tr>
+       <tr><th>Category</th><td>${response.data.category}</td></tr>
+       <tr><th>Continent</th><td>${response.data.weeks}</td></tr>
+       <tr><th>Cost</th><td>${response.data.cost}</td></tr>
+       <tr><th>About</th><td>${response.data.about}</td></tr>
+       `);
+  });
+});
+
+
 
 
 //
