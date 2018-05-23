26a2ef0c09e53b776d4dd50dc225fb7078aa84bd
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-22 20:10:17 -0700

    passing wave one - clicking button shows list of trips

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-
+const URL = "https://ada-backtrek-api.herokuapp.com/trips"
 
 const loadTrips = () => {
-  const tripList = $('#tripa-list');
+  const tripList = $('#trips-list');
   tripList.empty();
   axios.get(URL)
-    .then((response) => {
-
-    })
-    .catch((error) => {
-
+  .then((response) => {
+    const tripCollection = response.data
+    console.log(response);
+    tripCollection.forEach((trip) => {
+      console.log(trip);
+      tripList.append(`<li>${trip.name}</li>`)
     });
+  });
+  // .catch((error) => {
+  //
+  // });
 };
+
 
 
 
 
 $(document).ready (() => {
   $('#trips-button').click(loadTrips);
+  // loadTrips()
 });
