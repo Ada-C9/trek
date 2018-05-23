40c41c62e721071266ea93dc5c921ab5b67a876e
AngelaPoland <angelalpoland@gmail.com>
2018-05-22 17:10:07 -0700

    list of trips now prints to screen by pressing button

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
+
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+};
+
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
+// Loading Trips
+//
+const loadTrips = () => {
+  reportStatus('Loading trips...');
+
+  const tripList = $('#trip-list');
+  tripList.empty();
+
+  axios.get(URL)
+    .then((response) => {
+      reportStatus(`Successfully loaded ${response.data.length} trips`);
+      response.data.forEach((trip) => {
+        console.log(response);
+        tripList.append(`<li>${trip.name}</li>`);
+      });
+    })
+    .catch((error) => {
+      reportStatus(`Encountered an error while loading the trips: ${error.message}`);
+      console.log(error);
+    });
+};
+
+
+
+
+
+
+
+$(document).ready(() => {
+  $('#load').click(loadTrips);
+});
