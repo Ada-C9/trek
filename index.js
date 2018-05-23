8f9d73c42ccf3da5ddf843f24cae6fb12a1c00b2
Steffany Brown <steffannbrown@gmail.com>
2018-05-23 14:50:17 -0700

    changed css so reservation form is hidden

--- index.js
+++ index.js
@@ -31,29 +31,49 @@ const loadTrips = () => {
 
 
 const createReservation = (event) => {
-  // Note that createPet is a handler for a `submit`
-  // event, which means we need to call `preventDefault`
-  // to avoid a page reload
   event.preventDefault();
 
+  let tripID = $(`input[name="id"]`).val();
   let tripData = {
     name: $(`input[name="name"]`).val(),
     age: $(`input[name="age"]`).val(),
     email: $(`input[name="email"]`).val()
   };
 
-  axios.post(URL, petData)
+  axios.post(`${URL}/${tripID}`, tripData)
     .then((response) => {
       console.log('we successed it!')
     })
     .catch((error) => {
       console.log('dangit');
     });
-
 };
 
-const reservationForm = '<form id="trip-form"><div><label for="name">Name</label><input type="text" name="name"/></div><div><labelfor="age">Age</label><input type="number" name="age" /></div><div><label for="email">Email</label><input type="text" name="owner" /></div><input type="submit" name="add-reservation" value="Reserve"/></form>'
+const showTripDetails = (id) => {
+  // empty sections so you can add new content with click
+    const tripDetails = $('#trip-details');
+    tripDetails.empty();
+    const reserveTrip = $('#reserve-trip');
+    reserveTrip.empty();
 
+    let pageURL = TRIPSURL + `/${id}`
+    axios.get(pageURL)
+      .then((response) => {
+        tripDetails.append(`<p><h1>${response.data.name}</h1></p><p>${response.data.continent}</p><p><h3>About:</h3>${response.data.about}</p><p><h3>Category:</h3>${response.data.category}</p><p><h3>Weeks:</h3>${response.data.weeks}</p><p><h3>Cost:</h3>${response.data.cost}`);
+
+        reportStatus('Trip details loaded :)');
+      })
+        // axios is giving us this error object to use
+      .catch((error) => {
+        console.log(error);
+        reportStatus(`Error: ${error.message}`);
+      });
+}
+
+const buildReservationForm = (id) => {
+  $(".hidden-display").removeClass("hidden-display").addClass("show").attr("id",id)
+
+}
 
 
 $(document).ready(() => {
@@ -64,30 +84,12 @@ $(document).ready(() => {
     event.preventDefault();
     let id = $(this).attr('id');
 
-  // empty sections so you can add new content with click
-    const tripDetails = $('#trip-details');
-    tripDetails.empty();
-    const reserveTrip = $('#reserve-trip')
-    reserveTrip.empty();
+    showTripDetails(id);
+    buildReservationForm(id);
 
-    console.log('asdfalksj')
+    $(".hidden-display").removeClass("hidden-display").addClass("trip-form");
 
-    let pageURL = TRIPSURL + `/${id}`
-    axios.get(pageURL)
-      .then((response) => {
-        console.log(response.data)
-          tripDetails.append(`<p><h1>${response.data.name}</h1></p> <p>${response.data.continent}</p><p><h3>About: </h3>${response.data.about}</p><p> <h3>Category:</h3>${response.data.category}</p> <p><h3>Weeks: </h3>${response.data.weeks}</p><p><h3>Cost: </h3>${response.data.cost}`);
-
-        reserveTrip.append(`<p>${response.data.name}</p>` + reservationForm)
-
-        reportStatus('Trip details loaded :)');
-      })
-        // axios is giving us this error object to use
-      .catch((error) => {
-        console.log(error);
-        reportStatus(`Error: ${error.message}`)
-      });
-
-    $('#trip-form').submit(createReservation);
   });
+
+  $('#trip-form').submit(createReservation);
 });
