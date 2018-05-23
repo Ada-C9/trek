358de62c0b68cd167df92ea3e9e896a599ff03e6
emilcecarlisa <emilcecarlisa@gmail.com>
2018-05-23 11:46:14 -0700

    Added in reservation form

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const allTripsUrl = " https://ada-backtrek-api.herokuapp.com/trips";
 
 const singleTripUrl = "https://ada-backtrek-api.herokuapp.com/trips/";
 
+const reserveUrl = "https://ada-backtrek-api.herokuapp.com/trips/1/reservations"
+
 const reportStatus = (message) => {
   $('#status-message').html(message);
 };
@@ -19,7 +21,7 @@ const loadTrips = function loadTrips() {
     console.log('inside the .then');
     response.data.forEach((trip) => {
       console.log(trip);
-      tripList.append(`<li><a class="trip" href="#">${trip.name}</a></li>`);
+      tripList.append(`<li><a class="trip" data-id="${trip.id}" href="#">${trip.name}</a></li>`);
     });
     reportStatus('Trips Loaded!');
   })
@@ -40,16 +42,60 @@ const loadTrips = function loadTrips() {
 const getTrip = function getTrip(event) {
   // id, name, continent, about, category, weeks and cost
   // params to get id ???
+  console.log(event);
   event.preventDefault();
 
-  axios.get(singleTripUrl + '3' )
+  axios.get(singleTripUrl + $(this).data("id"))
   .then((response) => {
-    $('#details').empty().append(`<h2>${response.data.name}</h2><h3>${response.data.continent}</h3><p>${response.data.about}</p><p>${response.data.category}</p><p>${response.data.weeks}</p><p>${response.data.cost}</p>`);
+    $('#details').empty().append(`<h2>${response.data.name}</h2><h3>${response.data.continent}</h3><p>${response.data.about}</p><p>Category of trip: ${response.data.category}</p><p>Duration of trip: ${response.data.weeks}</p><p>$${response.data.cost}</p>`);
     reportStatus('Trips Loaded!');
   });
 }
 
+const FORM_FIELDS = ['name'];
+// below has the name attribute given to the input tag in the html form
+const inputField = name => $(`#trip-form input[name="${name}"]`);
+
+const readFormData = () => {
+  const getInput = name => {
+    const input = inputField(name).val();
+    return input ? input : undefined;
+  };
+  const formData = {};
+  // There is currently only one form field
+  FORM_FIELDS.forEach((field) => {
+    formData[field] = getInput(field);
+  });
+
+  return formData;
+};
+
+const clearForm = () => {
+  FORM_FIELDS.forEach((field) => {
+    inputField(field).val('');
+  });
+}
+
+const reserveTrip = (event) => {
+
+  event.preventDefault();
+
+  const reserveData = readFormData()
+  console.log(reserveData);
+
+  axios.post(reserveUrl, reserveData)
+    .then((response) => {
+      reportStatus(`Successfully added a reservation with ID ${response.data.id}!`);
+      clearForm();
+    })
+//     .catch(() => {
+//
+    // })
+};
+
 $(document).ready(() => {
   $('#load').click(loadTrips);
   $('.trips').on('click', '.trip', getTrip);
+  $('#trip-form').submit(reserveTrip);
+
 });
