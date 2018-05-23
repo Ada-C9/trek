5a2798a33c123c0c27f06d08511602a60d7464a0
Zheng Cao <chori84@gmail.com>
2018-05-22 23:54:41 -0700

    Implement Trip Details

--- index.js
+++ index.js
@@ -22,6 +22,35 @@ const loadTrips = () => {
     })
 }
 
+const loadTrip = (id) => {
+  const tripDetails = $('#trip-details');
+  tripDetails.empty();
+
+  reportStatus('Loading Trip Details! Please Wait...')
+
+  axios.get(URL + `/${id}`)
+    .then((response) => {
+      let data = response.data;
+      console.log(data.name);
+      tripDetails.append(`<h2>Trip Details</h2>`);
+      tripDetails.append(`<h3><strong>Name:</strong> ${data.name}</h3>`);
+      tripDetails.append(`<h3><strong>Continent:</strong> ${data.continent}</h3>`);
+      tripDetails.append(`<h3><strong>Category:</strong> ${data.category}</h3>`);
+      tripDetails.append(`<h3><strong>Weeks:</strong> ${data.weeks}</h3>`);
+      tripDetails.append(`<h3><strong>Cost:</strong> $${data.cost}</h3>`);
+      tripDetails.append(`<h3><strong>About:</strong></h3> <p>${data.about}</p>`);
+      console.log(tripDetails);
+      reportStatus('Trip Details Loaded!');
+    })
+    .catch((error) => {
+      reportStatus(`Error: ${error.message}`);
+    })
+}
+
 $(document).ready(() => {
   $('#load').click(loadTrips);
+  $('#trip-list').on('click', 'li', function() {
+    let id = $(this).attr('id');
+    loadTrip(id);
+  });
 })
