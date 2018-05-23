72db6388b862349b7c693b5d48186b3b6627fac4
SelamawitA <Selamawit.ainalem@gmail.com>
2018-05-23 11:31:56 -0700

    added functionality that allows user to click on trip for more details. New methods include: singleTripURL - path to API that requires trip id, loadTripdetails(), method that makes call to API with given trip ID passed in from click event, and .on() method that waits for a click event on the #all-trips list and passes in the list item to the loadTripdetails function.

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+//method for details
+//if a on click event occurs on a trip item
+//populate trip details list
+const URL = `https://ada-backtrek-api.herokuapp.com/trips`
 
 const reportStatus = (message) =>{
   $(`#status-message`).html(message);
 }
 
-const URL = `https://ada-backtrek-api.herokuapp.com/trips`
+let singleTripURL = function(tripID){
+  let singleURL = ``
+  return singleURL+=`https://ada-backtrek-api.herokuapp.com/trips/${tripID}`
+}
+
+const loadTripdetails = (aTrip)=>{
+  const tripDetails = $(`#trip-details`);
+  tripDetails.empty();
+
+  reportStatus('Searching for trip details...')
+
+  axios.get(singleTripURL(aTrip.target.id))
+    .then((response) => {
+        tripDetails.append(`<li>${response.data.name}</li>`)
+        tripDetails.append(`<li>${response.data.continent}</li>`)
+        tripDetails.append(`<li>${response.data.category}</li>`)
+        tripDetails.append(`<li>${response.data.weeks}</li>`)
+        tripDetails.append(`<li>${response.data.cost}</li>`)
+        tripDetails.append(`<li>${response.data.about}</li>`)
+      reportStatus('Trip details found!')
+    })
+  .catch((error) =>{
+      console.log(error)
+      reportStatus(`Error: ${error.message}`)
+    })
+}
 
 const loadTrips = ()=>{
   //create reference to all-trips element
@@ -15,8 +44,7 @@ const loadTrips = ()=>{
 axios.get(URL)
   .then((response) => {
     response.data.forEach((trip) => {
-      tripList.append(`<li id="trip-item">${trip.name}</li>`)
-
+      tripList.append(`<li id="${trip.id}">${trip.name}</li>`)
     });
     reportStatus('Trips loaded!');
   })
@@ -24,20 +52,15 @@ axios.get(URL)
     console.log(error)
     reportStatus(`Error: ${error.message} `)
   })
-
 }
 
-//method for details
-//if a on click event occurs on a trip item
-//populate trip details list
+
+
 
 $(document).ready(() => {
    $(`#load`).click(loadTrips);
-   $(document).on("click","#trip-item",function(){
-
+   $(document).on("click","#all-trips",function(event){
+     loadTripdetails(event);
    });
-   // $("trip-item").click(function(){
-   //   console.log('hello');
-   // });
 
 });
