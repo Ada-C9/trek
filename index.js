70479783a577675f98d19910c5807448fcef75d2
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-22 20:18:07 -0700

    added in user messaging

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const URL = "https://ada-backtrek-api.herokuapp.com/trips"
 
+const userMessage = (message) => {
+  $('#user-message').html(message)
+}
+
 const loadTrips = () => {
   const tripList = $('#trips-list');
   tripList.empty();
+  userMessage('Loading in trips..')
   axios.get(URL)
   .then((response) => {
     const tripCollection = response.data
-    console.log(response);
+    // console.log(response);
     tripCollection.forEach((trip) => {
-      console.log(trip);
+      // console.log(trip);
       tripList.append(`<li>${trip.name}</li>`)
     });
+    userMessage(`Showing ${tripCollection.length} amazing trips to choose from!`)
+  })
+  .catch((error) => {
+    userMessage(`Hrmm.. something has gone wrong: ${error.message}`)
+    console.log(error);
   });
-  // .catch((error) => {
-  //
-  // });
 };
 
 
