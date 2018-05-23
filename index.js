9ae7ef277642bd0958fd0060afa35a48fadfea74
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-05-23 16:26:02 -0700

    Completed initial styling of page. Fixed bug with All Trips header repeating on each load call.

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const URL = "https://ada-backtrek-api.herokuapp.com/trips";
 
+const reportStatus = function reportStatus(message) {
+  $('#status').html(message);
+};
+
 const loadTrips = function loadTrips() {
-  //reportStatus("Loading trips... be patient");
+  reportStatus("Loading trips... be patient");
+
+  const allTrips = $("#all-trips");
   const tripList = $("#trip-list");
+
+  allTrips.remove();
   tripList.empty();
+
 
   axios.get(URL)
     .then((response) => {
-      tripList.append("<h2>All Trips</h2>");
-
+      tripList.before("<h2 id='all-trips'>All Trips</h2>");
       response.data.forEach((trip) => {
         tripList.append(`<li id=${trip.id}>${trip.name}</li>`);
       });
-      //reportStatus(`Successfully loaded ${response.data.length} trips`);
-      //
+      reportStatus(`Successfully loaded ${response.data.length} trips`);
     })
     .catch((error) => {
-      //reportStatus(`Encountered an error while loading pets: ${error.message}`);
+      reportStatus(`Encountered an error while loading pets: ${error.message}`);
       console.log(error);
     });
 };
@@ -32,15 +39,18 @@ const loadTripDetails = function loadTripDetails() {
       let html = "";
 
       html += `<h2>Trip Details</h2>
-               <h3>Name: ${this.innerHTML}<h3>
-               <h4>Continent: ${trip.continent}</h4>
-               <h4>Category: ${trip.category}</h4>
-               <h4>Weeks: ${trip.weeks}</h4>
-               <h4>Cost: $${trip.cost}</h4>
-               <h4>About:<h4>
-               <p>${trip.about}</p>`;
+               <div>
+                 <h3>Name: ${this.innerHTML}<h3>
+                 <h4>Continent: ${trip.continent}</h4>
+                 <h4>Category: ${trip.category}</h4>
+                 <h4>Weeks: ${trip.weeks}</h4>
+                 <h4>Cost: $${trip.cost}</h4>
+                 <h4>About:</h4>
+                 <p>${trip.about}</p>
+               </div>`;
 
       tripDetails.append(html);
+      reportStatus(`Successfully loaded trip data for ${trip.name}`);
     })
     .catch((error) => {
       console.log(error);
@@ -54,13 +64,14 @@ const loadReserveTrip = function loadReserveTrip() {
 
   let html = "";
   html += `<h2>Reserve Trip</h2>
+           <h3>${this.innerHTML}</h3>
            <form id=${this.id}>
              <div>
-               <label for='name'>Your Name</label>
+               <label for='name'>Name:</label>
                <input type='text' id='name' />
              </div>
              <div>
-               <label for='email'>Email</label>
+               <label for='email'>Email:</label>
                <input type='text' id='email' />
              </div>
              <input type='submit' value='Reserve' />
@@ -69,7 +80,7 @@ const loadReserveTrip = function loadReserveTrip() {
   reserveTrip.append(html);
 };
 
-const makeReservation = (event) => {
+const makeReservation = function makeReservation(event) {
   event.preventDefault();
 
   const entryData = {
@@ -77,16 +88,15 @@ const makeReservation = (event) => {
     email: $("#email").val()
   };
 
-  const tripID = $("form")[0].id;
+  const tripID = event.target.id;
 
   axios.post(URL + `/${tripID}/reservations`, entryData)
     .then((response) => {
-      console.log("Success")
       console.log(response);
 
       $("#name").val(''),
       $("#email").val('')
-      //reportStatus("Successfully reserved a trip");
+      reportStatus("Successfully reserved a trip for ");
     })
     .catch((error) => {
       console.log(error.response);
