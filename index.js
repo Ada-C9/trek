42f25d9aaefbef78cf3b07b0e6267236622ec94b
AngelaPoland <angelalpoland@gmail.com>
2018-05-23 13:24:16 -0700

    showing individual trip details functionality works now.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
+const URL = 'https://ada-backtrek-api.herokuapp.com/trips/';
 
 const reportStatus = (message) => {
   $('#status-message').html(message);
@@ -24,19 +24,43 @@ const loadTrips = () => {
   tripList.empty();
 
   axios.get(URL)
-    .then((response) => {
-      reportStatus(`Successfully loaded ${response.data.length} trips`);
-      response.data.forEach((trip) => {
-        console.log(response);
-        tripList.append(`<li>
-          <button>${trip.name}</button></li>`);
+  .then((response) => {
+    reportStatus(`Successfully loaded ${response.data.length} trips`);
+
+    response.data.forEach((trip) => {
+      let item = $(`<li>${trip.name}</li>`);
+      item.click(function() {
+        getTripData(trip.id);
       });
-    })
-    .catch((error) => {
-      reportStatus(`Encountered an error while loading the trips: ${error.message}`);
-      console.log(error);
+      tripList.append(item);
     });
-};
+  })
+  .catch((error) => {
+    reportStatus(`Encountered an error while loading the trips: ${error.message}`);
+    console.log(error);
+  });
+
+}; // end of load Trips
+
+const getTripData = (trip_id) => {
+  const tripDetails = $(`#trip-details`);
+  tripDetails.empty();
+  let tripURL = URL + trip_id;
+
+  axios.get(tripURL)
+  .then((response) => {
+    console.log(response);
+    reportStatus(`Successfully loaded trip details for: ${response.data.name}`);
+    tripDetails.append(`<li>Trip Name: ${response.data.name}</li>`);
+    tripDetails.append(`<li>Travel Category: ${response.data.category}</li>`);
+    tripDetails.append(`<li>Continent: ${response.data.continent}</li>`);
+    tripDetails.append(`<li>Cost: $${response.data.cost}</li>`);
+    tripDetails.append(`<li>Weeks of Travel: ${response.data.weeks}</li>`);
+    tripDetails.append(`<li>About this trip: ${response.data.about}</li>`);
+  });
+}
+
+
 
 
 
