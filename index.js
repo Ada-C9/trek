09fb038fd06e971fc92026decd39ddc547c1e5c4
Cara <cararbrennan@gmail.com>
2018-05-23 11:40:11 -0700

    wave 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const URL = 'https://ada-backtrek-api.herokuapp.com/trips'
+const URL = 'https://ada-backtrek-api.herokuapp.com/trips/'
 
 
 const reportStatus = (message) => {
   $('status-message').html(message);
 };
 
-const reportError = (message, errors) =>{
+const reportError = (message, errors) => {
   let content = `<p>${message}</p><ul>`;
   for (const field in errors){
     for (const problem of errors[field]){
@@ -29,8 +29,45 @@ const loadTrips = () => {
     console.log(response);
     reportStatus(`Successfully loaded`);
     response.data.forEach((trip) => {
-      tripList.append(`<li>${trip.name}</li>`);
+
+      let id = trip.id
+
+      tripList.append(`<li class="trip" id="${id}">${trip.name}</li>`);
+
+
     });
+    //only returns one element
+
+  })
+
+  .catch((error) => {
+    reportStatus(`Encountered an error while loading trips: ${error.message}`);
+    reportError(error.message, error.response.data.errors)
+    console.log(error);
+
+  });
+
+};
+
+const clickTrip = (trip) => {
+  console.log(trip)
+
+  axios.get(URL+trip)
+  .then((response) => {
+    console.log(response);
+    reportStatus(`Successfully loaded`);
+
+    console.log('appending to table body');
+
+    $('tablebody').append(`<tr><th>Name</th><td>${response.data.name}</td></tr>`);
+
+    $('tablebody').append(`<tr><th>Id</th><td>${response.data.id}</td></tr>`); $('tablebody').append(`<tr><th>Continent</th><td>${response.data.continent}</td></tr>`);  $('tablebody').append(`<tr><th>About</th><td>${response.data.about}</td></tr>`);
+    $('tablebody').append(`<tr><th>Category</th><td>${response.data.category}</td></tr>`);
+    $('tablebody').append(`<tr><th>Weeks</th><td>${response.data.weeks}</td></tr>`);
+    $('tablebody').append(`<tr><th>Cost</th><td>${response.data.cost}</td></tr>`);
+
+
+
   })
 
   .catch((error) => {
@@ -39,9 +76,21 @@ const loadTrips = () => {
     console.log(error);
 
   });
+
 };
+
+
+
+
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
+  $('ul').on('click', 'li', function() {
+    console.log(this)
+    console.log(this.id)
+    clickTrip(this.id)
+  });
+
+
 
 });
