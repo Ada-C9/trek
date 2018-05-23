95afe76d3b11f57e41fe7b42e3a3930b809ae2fd
Mary Lamkin <mmlamkin@gmail.com>
2018-05-23 15:34:57 -0700

    adds some styling but reserve is broken?

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const loadTrips = () => {
 
   axios.get(URL)
   .then((response) => {
+    Trips.append(`<h1>All Trips</h1>`);
     response.data.forEach((trip) => {
       Trips.append(`<li id='${trip.id}'>${trip.name}</li>`);
     });
@@ -19,36 +20,37 @@ const loadTrips = () => {
   })
 
   .catch((error) => {
-    console.log(error);
+
     reportStatus(`Error: ${error.message}`)
   });
 };
 
 const loadOneTrip = (id) => {
   const TripURL = 'https://ada-backtrek-api.herokuapp.com/trips/'
-  axios.get(`${TripURL}${id}`)
-  .then((response) => {
-
-    $('#trip').addClass(id)
-    $('#trip').append(`<h1>Trip Details</h1>`);
-    $('#trip').append(`<h3>Name: <span>${response.data.name}</span></h3>`);
-    $('#trip').append(`<p>Category: ${response.data.category}</p>`);
-    $('#trip').append(`<p>Weeks: ${response.data.weeks}</p>`);
-    $('#trip').append(`<p>Cost: $${response.data.cost}</p>`);
-    $('#trip').append(`<p>About: ${response.data.about}</p>`);
-    LoadReservationForm(id)
-  })
-  .catch((error) => {
-    console.log(error);
-    reportStatus(`Error: ${error.message}`)
-  });
+  if (!$('#trip').hasClass('occupied')) {
+    axios.get(`${TripURL}${id}`)
+    .then((response) => {
+      $('#trip').addClass('occupied')
+      $('#trip').addClass(id)
+      $('#trip').append(`<h1>Trip Details</h1>`);
+      $('#trip').append(`<h3> <span>${response.data.name}</span></h3>`);
+      $('#trip').append(`<p>Category: ${response.data.category}</p>`);
+      $('#trip').append(`<p># of Weeks: ${response.data.weeks}</p>`);
+      $('#trip').append(`<p>Cost: $${response.data.cost.toFixed(2)}</p>`);
+      $('#trip').append(`<p>About: ${response.data.about}</p>`);
+      LoadReservationForm(id)
+    })
+    .catch((error) => {
+      reportStatus(`Error: ${error.message}`)
+    });
+  }
 };
 
 const LoadReservationForm = (id) => {
   $('#reservations').prepend(`<h1>Reserve This Trip</h1>`);
-  $('.name label').html('Name');
+  $('.name label').html('Name: ');
   $('.name label').append('<input type="text" name="name" />');
-  $('.email label').html('Email');
+  $('.email label').html('Email: ');
   $('.email label').append('<input type="text" name="email" />');
   $('.trip-name label').html(`Trip: ${$('#trip span').html()}`);
   $('#reserve').append('<input type="submit" name="reserve-trip" value="Reserve Trip" />')
@@ -82,7 +84,6 @@ const reserveTrip = (event) => {
 
   clearForm();
 }
-
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
