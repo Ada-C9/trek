02123820a5bc536925492be40e86d21ae7f24a1b
Maja Graonic <m.graonic@gmail.com>
2018-05-23 16:03:35 -0700

    basic functionality done

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-
-const reportStatus = (content) => {
-  $('status-message').html(content);
+const reportStatus = (message) => {
+  $('#status-message').html(message);
 };
-
 const reportErrors = (message, errors) => {
   let content = `<p>${message}</p><ul>`;
   for (const name in errors) {
@@ -15,51 +13,110 @@ const reportErrors = (message, errors) => {
 };
 
 const loadTrips = () => {
-  const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
   reportStatus('Loading trips . . .')
+
+  const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
 
   let tripList = $('#all-trips');
   tripList.empty();
 
   axios.get(URL)
-
-    .then((response) => {
-      response.data.forEach((response) => {
-        let id = response.id;
-        let tripName = response.name;
-        // tripList.append(`<li><a href="https://ada-backtrek-api.herokuapp.com/trips/${id}">${tripName}</li>`);
-        tripList.append(`<li class="trip-link" id="${id}">${tripName}</li>`);
-        $(`#${id}`).click(() => {
-          displayTrip(id);
-        })
-      });
-    })
-
-    .catch((error) => {
-      reportStatus(`Had trouble loading trips: ${error.message}`);
-      console.log(error);
-    })
+  .then((response) => {
+    response.data.forEach((response) => {
+      let id = response.id;
+      let tripName = response.name;
+      tripList.append(`<li class="trip-link" id="${id}">${tripName}</li>`);
+    });
+    reportStatus(`Loaded ${response.data.length} trips!`)
+  })
+  .catch((error) => {
+    reportStatus(`Had trouble loading trips: ${error.message}`);
+    console.log(error);
+  });
 };
 
-const displayTrip = (tripID) => {
+const displayTripInfo = (tripID) => {
   const URL = 'https://ada-backtrek-api.herokuapp.com/trips/' + `${tripID}`;
+  axios.get(URL)
+  .then ((response) => {
+    let tripData = response.data;
+    // let tripName = response.data.name;
+    // tripDetails.html(tripName);
+    // id, name, continent, about, category, weeks and cost
+    buildDetails(tripData);
+    buildReservationBox(tripData);
+  })
+  .catch((error) => {
+    reportStatus(`Had trouble loading trips: ${error.message}`);
+    console.log(error);
+  });
+};
+
+const readFormData = () => {
+  const formData = {};
+  formData['name']= $('form input[name="name"]').val();
+  formData['email']= $('form input[name="email"]').val();
+  return formData;
+};
+
+const reserveTrip = (event) => {
+  const tripID = $('form input[type="submit"]').data('trip-id');
+  const URL = 'https://ada-backtrek-api.herokuapp.com/trips/' + `${tripID}` + '/reservations';
+  event.preventDefault();
+
+  const tripData = readFormData();
+  $('form')[0].reset();
+
+  axios.post(URL, tripData)
+  .then((response) => {
+    console.log(response.data);
+    reportStatus(`Successfully reserved trip #${response.data.trip_id}! `);
+  })
+  .catch((error) => {
+    reportStatus(`Had trouble loading trips: ${error.message}`);
+    console.log(error);
+  });
+};
+
+const buildReservationBox = (tripData) => {
+  let tripID = tripData.id;
+  let tripName = tripData.name;
+  let reservationBox = $('#reserve-trip-form');
+  let details = '<h3>Reserve Trip</h3>';
+
+  details += '<div>Name: <input type="text" name="name" placeholder="First Last"></div>';
+  details += '<div>Age: <input type="text" name="age" placeholder="Age"></div>';
+  details += '<div>Email: <input type="text" name="email" placeholder="Email"></div>';
+  details += `<input type="submit" data-trip-id="${tripID}" name="reserve-trip" value="Reserve Trip">`;
+
+  reservationBox.html(details);
+};
+
+
+
+const buildDetails = (tripData) => {
   let tripDetails = $('#trip-details');
 
-  axios.get(URL)
-    .then ((response) => {
-      let tripName = response.data.name;
-      tripDetails.html(tripName);
-    })
-    .catch((error) => {
-      reportStatus(`Had trouble loading trips: ${error.message}`);
-      console.log(error);
-    })
+  let details = '<h3>Trip Details</h3><ul>';
+  details += '<li>' + tripData.name + '<li>';
+  details += '<li>' + tripData.continent + '<li>';
+  details += '<li>' + tripData.category + '<li>';
+  details += '<li>' + tripData.weeks + '<li>';
+  details += '<li>' + tripData.cost + '<li>';
+  details += '<li>About: <li>';
+  details += '<p>' + tripData.about + '<p>';
+  details += '<ul>';
+
+  tripDetails.html(details);
 };
 
+
 $(document).ready(() => {
+  // Use Foundation CDN!!
   $('#load-trips').click(loadTrips);
-  // $('body').on('click', '.trip-link', function(event){
-  //   let trip = event.target.id;
-  //   (displayTrip(trip));
-  // });
+  $('#all-trips').on('click', '.trip-link', function(event){
+    let trip = event.target.id;
+    (displayTripInfo(trip));
+  });
+  $('#reserve-trip-form').submit(reserveTrip);
 });
