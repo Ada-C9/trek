a541928bfa7109ae0db297dc05422df0fae9a8e7
Phoebe <phoebe.manning8@gmail.com>
2018-05-23 09:58:53 -0700

    basic logic for displaying single trip

--- index.js
+++ index.js
@@ -29,17 +29,33 @@ const showTrip = (event) => {
   reportStatus('Loading Trip...');
   console.log(event.target.id);
   const tripId = event.target.id;
+  const tripSection = $('#show-trip').empty();
+
   // console.log('Trip clicked');
   axios.get(URL + `/${tripId}`)
     .then((response) => {
-      console.log(response.data.name);
-      $('#show-trip').text(response.data.name); //CALL NEW FUNCTION TO BUILD HTML
+      console.log(response.data);
+      // $('#trip-name').text(response.data.name); //CALL NEW FUNCTION TO BUILD HTML
+      // tripSection.append(buildTrip(response.data));
+      let html = buildTrip(response.data);
+      tripSection.append(html);
       reportStatus(`Trip #${tripId} Loaded!`);
     })
     .catch((error) => {
       console.log(error.message);
       reportStatus(`Error: ${error.message}`);
     });
+};
+
+const buildTrip = (tripData) => {
+  return (`<div>
+            <h3>Name: ${tripData.name} </h3>
+            <p>Continent: ${tripData.continent} </p>
+            <p>Category: ${tripData.category} </p>
+            <p>Weeks: ${tripData.weeks} </p>
+            <p>Cost: $${tripData.cost} </p>
+            <p>About: ${tripData.about} </p>
+          </div>`);
 };
 
 $(document).ready(() => {
