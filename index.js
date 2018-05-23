6f41f27a25d1a06f8f6d8531770600fc297d3ec4
Aruna <aruna.thungathurthi@gmail.com>
2018-05-23 15:38:33 -0700

    implemented all trips

---
+++ index.js
@@ -0,0 +1,27 @@
+const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
+
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+}
+
+const loadTrips = () => {
+  const tripList = $('#trip-list');
+  tripList.empty();
+
+  reportStatus('Loading Trips! Please Wait...');
+
+  axios.get(URL)
+    .then((response) => {
+      response.data.forEach((trip) => {
+        tripList.append(`<li id="${trip.id}">${trip.name}</li>`);
+      });
+      reportStatus('Trips Loaded!');
+    })
+    .catch((error) => {
+      reportStatus(`Error: ${error.message}`);
+    })
+}
+$(document).ready(() => {
+  $('#load').click(loadTrips);
+
+  });
