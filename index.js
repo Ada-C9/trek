6bc3214293159c61196324b353657d63a7cbeb2e
marikoja <Marikoanderson@gmail.com>
2018-05-22 17:10:28 -0700

    base page with button to show all trips

---
+++ index.js
@@ -0,0 +1,33 @@
+// url to retrieve all trips
+const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
+
+const reportStatus = (message) => {
+  $('status-message').html(message);
+}
+
+const loadTrips = () => {
+  reportStatus('Loading Trips! Thank you for waiting...');
+
+  const tripList = $('#trip-list');
+  tripList.empty();
+
+  // get request to API
+  axios.get(URL)
+    // success vv
+    .then((response) => {
+      response.data.forEach((trip) => {
+        console.log(response);
+        tripList.append(`<li>${trip.name}</li>`);
+      });
+    reportStatus('Trips Loaded');
+    })
+    // fail vv
+    .catch((error) => {
+      console.log(error);
+      reportStatus(`Error: ${error.message}`);
+    });
+}
+
+$(document).ready(() => {
+  $('#load').click(loadTrips);
+})
