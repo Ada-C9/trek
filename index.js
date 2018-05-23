c4f6bdbf78a05c499a77f76d4270a8a41226182f
Ari <dominiquearia0@gmail.com>
2018-05-22 19:49:18 -0700

    loadTrip function for trip detail

--- index.js
+++ index.js
@@ -2,36 +2,36 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
-const reportError = (message, errors) => {
-  let content = `<p>${message}</p><ul>`;
-  for (const field in errors) {
-    for (const problem of errors[field]) {
-      content += `<li>${field}: ${problem}</li>`;
-    }
-  }
-  content += "</ul>";
-  reportStatus(content);
-};
+// const reportError = (message, errors) => {
+//   let content = `<p>${message}</p><ul>`;
+//   for (const field in errors) {
+//     for (const problem of errors[field]) {
+//       content += `<li>${field}: ${problem}</li>`;
+//     }
+//   }
+//   content += "</ul>";
+//   reportStatus(content);
+// };
 
-const url = 'https://ada-backtrek-api.herokuapp.com/trips'
+const allTrips = 'https://ada-backtrek-api.herokuapp.com/trips/'
 
 const loadTrips = () => {
   reportStatus('Loading trips...');
-
   const tripList = $('#trip-list');
   tripList.empty();
 
-  axios.get(url)
+  axios.get(allTrips)
     .then((response) => {
       reportStatus(`Successfully loaded trips`);
+
       let results = response.data
+      tripList.append('<h3>All Trips</h3>')
       results.forEach((result) => {
-        let html = '<li>';
-        html += `<strong>ID: ${result.id}</strong>`;
-        html += ` Trip: ${result.name}`;
-        html += ` Continent: ${result.continent}`;
-        html += ` Price: $${result.cost}`;
-        html += `</li><br></br>`;
+        let html = `<li>`;
+        html += `<strong>ID: ${result.id}</strong></li>`;
+        html += `<li><a class="link" href="${result.id}">${result.name}</a></li>`;
+
+        html += `<br></br>`;
         tripList.append(html)
       });
     })
@@ -41,8 +41,37 @@ const loadTrips = () => {
     })
 };
 
+const loadTrip = () => {
+  reportStatus('Loading trip..');
+  const tripDetail = $('#trip-detail');
+  tripDetail.empty();
+
+  let tripId = $('.link')[0].getAttribute('href')
+  let tripUrl = (allTrips + `${tripId}`)
+
+  axios.get(tripUrl)
+    .then((response) => {
+      reportStatus(`Successfully loaded trip`);
+      tripDetail.append('<h3>Trip Detail:</h3>')
+
+        let html = `<li><strong>ID:
+        ${response.id}</strong></li>`;
+        html += `<li>Trip: ${response.name}</li>`;
+        html += `<li>Continent: ${response.continent}`;
+        html += `<li>About: ${response.about}`;
+        html += `<li>Weeks: ${response.weeks}`;
+        html += `<li>Price: ${response.cost}`;
+        html += `<br></br>`;
+        tripDetail.append(html)
+    })
+    .catch((error) => {
+      reportStatus(`Encountered an error while loading trip: ${error.message}`);
+      console.log(error);
+    })
+};
 
 
 $(document).ready(() => {
-  $('#load').click(loadTrips());
+  $('#load').click(loadTrips);
+  $('a').on('click', loadTrip);
 });
