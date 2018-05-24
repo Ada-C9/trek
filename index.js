4ece9e1231cdfbf84809ae3f237e7e29a913519c
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-05-24 09:51:28 -0700

    added css, started reserve functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
 
+
 const reportStatus = (message) => {
   $('#status-message').html(message);
 };
@@ -24,6 +25,7 @@ const loadtrips = () => {
   axios.get(URL)
   .then((response) => {
     reportStatus(`Successfully loaded ${response.data.length} trips`);
+    tripList.append(`<h3>All Trips</h3>`)
     response.data.forEach((trip) => {
       tripList.append(`<li trip-id="${trip.id}"><a>${trip.name}</a></li>`);
     });
@@ -47,26 +49,80 @@ const viewTrip = (id) => {
     reportStatus(`Successfully loaded trip #${id}`);
     let trip = response.data;
     let tripDetails = `
-    <p> Id: ${trip.id} </p>
-    <p> Name: ${trip.name} </p>
+    <h3> ${trip.name} - Trip Details </h3>
     <p> Continent: ${trip.continent} </p>
-    <p> About: ${trip.about} </p>
     <p> Category: ${trip.category} </p>
     <p> Duration: ${trip.weeks} weeks</p>
-    <p> Cost: ${trip.cost} </p>`;
+    <p> Cost: $${trip.cost} </p>
+    <p> ${trip.about} </p>
+    `;
     $('#trip-details').append(tripDetails);
+
+
   })
 };
 
+const FORM_FIELDS = ['name', 'age', 'email'];
+const inputField = name => $(`#reservation-form input[name="${name}"]`);
+
+const readFormData = () => {
+  const getInput = name => {
+    const input = inputField(name).val();
+    return input ? input : undefined;
+  };
+
+  const formData = {};
+  FORM_FIELDS.forEach((field) => {
+    formData[field] = getInput(field);
+  });
+
+  return formData;
+};
+
+const clearForm = () => {
+  FORM_FIELDS.forEach((field) => {
+    inputField(field).val('');
+  });
+}
+
+const reserveTrip = (event) => {
+  event.preventDefault();
+  const reservationURL = URL + '/' + tripId + '/reservations';
+
+  const reservationData = readFormData();
+  console.log(reservationData);
+
+  reportStatus('Reserving trip...');
+
+  axios.post(reservationURL, reservationData)
+  .then((response) => {
+    reportStatus(`Successfully added a reservation with ID ${response.data.id}!`);
+    clearForm();
+  })
+  .catch((error) => {
+    console.log(error.response);
+    if (error.response.data && error.response.data.errors) {
+      reportError(
+        `Encountered an error: ${error.message}`,
+        error.response.data.errors
+      );
+    } else {
+      reportStatus(`Encountered an error: ${error.message}`);
+    }
+  });
+};
 
 $(document).ready(() => {
-  $('#load').click(loadtrips);
+  $("#trip-location").hide();
+  $("#new-reservation").hide();
+  $('#load').click(loadtrips)
   $('ul').on('click', 'li', function() {
     let tripID = $(this).attr('trip-id');
     viewTrip(tripID);
-    $('#tripLocation').show();
+    $('#trip-location').show();
+    // $('reservation-form').show();
+    // $('reservation-form').attr('tripid', trip.id);
+    // $('#reservation-form').submit(reserveTrip);
   });
-
-  // $('#trip-form').submit(createTrip);
-
+  $('#reservation-form').submit(reserveTrip);
 });
