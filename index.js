057fe59d938f0f5d1674f849037e3f11ccafc270
Tor Shimizu <torshimizu@gmail.com>
2018-05-24 00:56:33 -0700

    updated all trips code to report errors and to style trips as a table

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p>`;
+  content += '<ul>';
+
+  for (const field in errors ){
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += '</ul>';
+  reportStatus(content);
+
+};
+
 const tableHeaders = (trip) => {
   let headers = '';
   headers += '<thead> <tr>';
+  console.log(Object.keys(trip));
   Object.keys(trip).forEach( (header) => {
-    headers += `<th>${header}</th>`;
+    headers += `<th>${header.toUpperCase()}</th>`;
   });
   headers += '</tr> </th>';
 
@@ -20,16 +35,37 @@ const getTrips = () => {
   tripList.empty();
 
   reportStatus('Loading trips...')
+
   axios.get(baseURL)
-    .then( (response) => {
+    .then((response) => {
+      $('#trip-list').append(tableHeaders(response.data[0]));
+      $('#trip-list').append('<tbody>');
+
       response.data.forEach( (trip) => {
-        let content = '';
-        content += `<li>${trip.id}: ${trip.name}</li>`
-        content += `<li>${trip.continent}`
+        let content = '<tr>';
+        content += `<td>${trip.id}`;
+        content += `<td>${trip.name}</td>`;
+        content += `<td>${trip.continent}</td>`;
+        content += `<td>${trip.category}</td>`;
+        content += `<td>${trip.weeks}</td>`;
+        content += `<td>${trip.cost}</td>`;
+        content += `</tr>`;
+        $('#trip-list').append(content);
 
-
-        $('#trip-list').append()
       });
+
+      $('#trip-list').append('</tbody>');
+      reportStatus(`${response.data.length} trips loaded`);
+    })
+    .catch((error) => {
+      if (error.response.data && error.response.data.errors) {
+        reportError(
+          `Encountered an error: ${error.message}`,
+          error.response.data.errors
+        );
+      } else {
+        reportStatus(`Error: ${error.message }`);
+      }
     });
 
 };
@@ -37,6 +73,6 @@ const getTrips = () => {
 
 
 $(document).ready( () => {
-  $('#load').click()
+  $('#load').click(getTrips);
 
 });
