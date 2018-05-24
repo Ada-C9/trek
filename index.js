eee9c306f19448583c085f79ee6b195f6efd3aff
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-24 15:40:26 -0700

    finished basic logic to handle submission of a trip reservation with only nominal information and next onto edge case and css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready( () => {
   const URL = "https://ada-backtrek-api.herokuapp.com/trips"
   $('#trips-button').click(() => {
+    $('main').html($('#trips-button'))
     $('main').append('<table id="trips-container"><tr><th>All Trips</th></tr></table>');
     axios.get(URL)
     .then((response) => {
@@ -26,6 +27,22 @@ $(document).ready( () => {
           <p><strong>Cost:</strong> ${trip.cost}</p>
           <p><strong>About:</strong></p><p>${trip.about}</p>`
         );
+        $('span#specific-trip').html(` ${trip.name}`)
+        $('span#specific-trip').attr('class', `${trip.id}`)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  });
+  $('aside').on('submit', '#reserve-form', function(event) {
+    event.preventDefault();
+    const id = $('span#specific-trip').attr('class');
+    const name = $('input#name').val();
+    const email = $('input#email').val();
+    const reservationData = {name: name, email: email}
+    axios.post(`${URL}/${id}/reservations`, reservationData)
+    .then((response) => {
+      console.log(response);
     })
     .catch((error) => {
       console.log(error);
