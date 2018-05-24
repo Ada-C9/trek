c7eeeb0718176a9a91fa623dff063e53999d02c1
LetiTran <leticiastran@gmail.com>
2018-05-23 17:00:35 -0700

    Hidding body until requested to load trips. Needs more work on this tho.

--- index.js
+++ index.js
@@ -56,16 +56,17 @@ const loadATrip = (tripID) => {
     reportStatus(`Successfully loaded <strong>${response.data.name}</strong> trip details`);
     // response.data.forEach((trip) => {
     tripDetails.append(`<h3>${response.data.name}</h3>
-      <h5>Continent</h5><p>${response.data.continent}</p>
-      <h5>Category</h5><p>${response.data.category}</p>
-      <h5>Weeks</h5><p>${response.data.weeks}</p>
-      <h5>Cost</h5><p>${response.data.cost}</p>
-      <h5>About</h5><p>${response.data.about}</p>`);
+      <div><h5>Continent</h5><p>${response.data.continent}</p></div>
+      <div><h5>Category</h5><p>${response.data.category}</p></div>
+      <div><h5>Weeks</h5><p>${response.data.weeks}</p></div>
+      <div><h5>Cost</h5><p>$ ${response.data.cost.toFixed(2)}</p></div>
+      <div><h5>About</h5><p>${response.data.about}</p></div>`);
       // });
 
       // Trip name on the 'reserve trip' section:
-      $('#trip-name').html(response.data.name);
+      // $('#trip-name').html(response.data.name);
       $('#trip-id').html(response.data.id);
+      $('#makeReservation-button').val(`Reserve ${response.data.name} Trip!`)
     })
     .catch((error) => {
       reportStatus(`Encountered an error while loading trips: ${error.message}`);
@@ -124,58 +125,46 @@ const loadATrip = (tripID) => {
       if (error.response.data && error.response.data.errors) {
         reportError(`Encountered an error: ${error.message}`,
           error.response.data.errors);
-        // $(`#reservation-message`).html(`put error....!`)
-      } else {
-        // reportStatus(`Encountered an error: ${error.message}`);
-        $(`#reservation-message`).html(`Encountered an error: ${error.message}`)
-      }
+          // $(`#reservation-message`).html(`put error....!`)
+        } else {
+          // reportStatus(`Encountered an error: ${error.message}`);
+          $(`#reservation-message`).html(`Encountered an error: ${error.message}`)
+        }
+      });
+    };
+
+
+
+    // ______________________________________________________
+    // EVENTS:
+    $(document).ready(() => {
+
+      // Load Trips:
+      $('#load').click(loadTrips);
+
+      // Load details of a specific trip:
+      $('#trip-list').on('click', 'li', function(event) {
+        console.log($(this)[0].id);
+        loadATrip($(this)[0].id);
+      });
+
+      // Reserve a Trip:
+      $('#reserve-block').submit(reserve);
+
+      // Load foundation on entire document
+      // this is where I was getting that Modal error.
+      $(document).foundation();
+
+      // Display rest of body only after the load trips button is clicked on:
+      $('#load').on('click', function(event) {
+        $(".cell.medium-auto.medium-cell-block-container").css("display", "contents");
+      });
+
+      // $('li.trip').on('click', function(event) {
+      //   $(".trip-details").css("display", "contents");
+      // });
+
+
     });
 
-
-  };
-
-  // $('.reveal').html(`<p id="reservation-message">Successfully created a reservation with id ${response.data.trip_id}!</p>`)
-
-
-  // ______________________________________________________
-  // EVENTS:
-  $(document).ready(() => {
-
-    // Load Trips:
-    $('#load').click(loadTrips);
-
-    // Load details of a specific trip:
-    $('#trip-list').on('click', 'li', function(event) {
-      console.log($(this)[0].id);
-      loadATrip($(this)[0].id);
-    });
-
-    // Reserve a Trip:
-    $('#reserve-block').submit(reserve);
-
-    // Load foundation on entire document
-    // this is where I was getting the Modal error.
-    $(document).foundation();
-
-
-
-  });
-
-
-
-
-
-
-  // PAGINATION .... ??
-
-  // $('#trip-list').pagination({
-  //     dataSource: [1, 2, 3, 4, 5, 6, 7, 50],
-  //     pageSize: 5,
-  //     showPageNumbers: false,
-  //     showNavigator: true,
-  //     callback: function(data, pagination) {
-  //         // template method of yourself
-  //         let html = template(data);
-  //         dataContainer.html(html);
-  //     }
-  // })
+    // ______________________________________________________
