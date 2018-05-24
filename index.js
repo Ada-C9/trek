c1625eb12d70c64565680bc29d723f0082c76a09
emilcecarlisa <emilcecarlisa@gmail.com>
2018-05-23 20:19:17 -0700

    Added email input field

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const allTripsUrl = " https://ada-backtrek-api.herokuapp.com/trips";
 const singleTripUrl = "https://ada-backtrek-api.herokuapp.com/trips/";
 
 const reserveUrl = "https://ada-backtrek-api.herokuapp.com/trips/1/reservations"
-
 const reportStatus = (message) => {
   $('#status-message').html(message);
 };
@@ -46,22 +45,22 @@ const getTrip = function getTrip(event) {
   .then((response) => {
     $('#details').empty().append(`<h2>${response.data.name}</h2><h3>${response.data.continent}</h3><p>${response.data.about}</p><p>Category of trip: ${response.data.category}</p><p>Duration of trip (in weeks): ${response.data.weeks}</p><p>$${response.data.cost}</p>`);
     reportStatus('Trips Loaded!');
-
-    // call style.display on new-trip section tag
-    // when person clicks on trip title(below in doc ready), they alsoset of the submit
   });
 }
 
-const FORM_FIELDS = ['name'];
+const FORM_FIELDS = ['name', 'email'];
+// this looks like a function, but does the var(?) name below have to match the name when you call it below --like this inputField(name)
 const inputField = name => $(`#trip-form input[name="${name}"]`);
 
+// Why is the function expression left without a name on the right side of the equal sign?
 const readFormData = () => {
+// Is getInput the variable that holds an anonymous function called name, which takes no arguments? Why no empty parens?
   const getInput = name => {
+// input holds the resulting value from calling inputField function
     const input = inputField(name).val();
     return input ? input : undefined;
   };
   const formData = {};
-  // There is currently only one form field
   FORM_FIELDS.forEach((field) => {
     formData[field] = getInput(field);
   });
@@ -82,21 +81,15 @@ const reserveTrip = (event) => {
 
   let display = $('#new-reserve');
   display.show();
-//
-//   // if (display === 'none') {
-//   //   display = 'block';
-//   // } else {
-//   //   display = 'none';
-//   // }
-//
-//   const reserveData = readFormData()
-//   console.log(reserveData);
-//
-//   axios.post(reserveUrl, reserveData)
-//     .then((response) => {
-//       reportStatus(`Successfully added a reservation with ID ${response.data.id}!`);
-//       clearForm();
-//     })
+
+  const reserveData = readFormData()
+  console.log(reserveData);
+
+  axios.post(reserveUrl, reserveData)
+    .then((response) => {
+      reportStatus(`Successfully added a reservation with ID ${response.data.id}!`);
+      clearForm();
+    })
 // //     .catch(() => {
 // //
 //     // })
@@ -109,6 +102,6 @@ $(document).ready(() => {
   $('.trips').on('click', '.trip', reserveTrip);
 
   // $('#new-reserve').on('click', '.trip', reserveTrip);
-  // $('#trip-form').submit(reserveTrip);
+  $('#trip-form').submit(reserveTrip);
 
 });
