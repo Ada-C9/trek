d4dbd810dc5c7264f87e15b561a1ec17b64c7f29
Nora Peters <norapeters5@gmail.com>
2018-05-24 11:17:30 -0700

    adds event listener for submit event, and functions to read and format form data and successfully submit axios post request

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 }
 
+const reportError = (message, errors) => {
+  let content = `<p>${message}</p><ul>`;
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
+
 const loadTrips = () => {
   const tripList = $('#trip-list');
   tripList.empty();
@@ -12,12 +23,8 @@ const loadTrips = () => {
 
   axios.get(URL)
     .then((response) => {
-      console.log(response);
       response.data.forEach((trip) => {
         let item = $(`<li>${trip.name}</li>`).attr('id', `${trip.id}`);
-        // item.click(function() {
-        //   getTripData(trip.id);
-        // });
         tripList.append(item);
       });
       reportStatus(`Successfully loaded ${response.data.length} trips`)
@@ -26,7 +33,6 @@ const loadTrips = () => {
       console.log(error);
       reportStatus(`Error: ${error.message}`);
     });
-    console.log(tripList);
 };
 
 const getTripDetails = function getTripDetails(id) {
@@ -37,19 +43,71 @@ const getTripDetails = function getTripDetails(id) {
   axios.get(URL + id)
     .then((response) => {
       let data = response.data;
-      let name = $(`<p><strong>Name:</strong> ${data.name}</h4>`);
+      let name = $(`<h4><strong>Name:</strong> ${data.name}</h4>`).addClass(`${id}`);
+      let about = $(`<p><strong>Description:</strong> ${data.about}</p>`)
       let continent = $(`<p><strong>Continent:</strong> ${data.continent}</p>`)
       let category = $(`<p><strong>Category:</strong> ${data.category}</p>`)
       let weeks = $(`<p><strong>Weeks:</strong> ${data.weeks}</p>`)
-      let cost = $(`<p><strong>Cost:</strong> ${data.cost}</p>`)
+      let cost = $(`<p><strong>Cost:</strong> $${data.cost}</p>`)
 
-      tripDetail.append(name, continent, category, weeks, cost);
-      // $('#trip-detail-section').append(`<h4>Name: ${data.name}</h4>`);
+      tripDetail.append(name, about, continent, category, weeks, cost);
+
       reportStatus(`Successfully loaded details for ${response.data.name} trip`)
     })
     .catch((error) => {
       console.log(error);
       reportStatus(`Error: ${error.message}`);
+    });
+}
+
+const FORM_FIELDS = ['name', 'email'];
+const inputField = name => $(`#trip-form input[name="${name}"]`);
+
+const readFormData = () => {
+  const getInput = name => {
+    const input = inputField(name).val();
+    return input ? input : undefined;
+  };
+
+  const formData = {};
+  FORM_FIELDS.forEach((field) => {
+    formData[field] = getInput(field);
+  });
+
+  return formData;
+};
+
+const clearForm = () => {
+  FORM_FIELDS.forEach((field) => {
+    inputField(field).val('');
+  });
+}
+
+const reserveTrip = (event) => {
+  event.preventDefault();
+
+  // let id = Number($('#trip-detail-section h4')[0].classList[0]);
+  let id = $('.trip-detail-section h4').attr("class");
+
+  const tripData = readFormData();
+
+  reportStatus('Sending trip reservation data...');
+
+  axios.post((`${URL}${id}/reservations`), tripData)
+    .then((response) => {
+      clearForm();
+      reportStatus(`Successfully created trip reservation with ID ${response.data.trip_id}!`);
+    })
+    .catch((error) => {
+      console.log(error.response);
+      if (error.response.data && error.response.data.errors) {
+        reportError(
+          `Encountered an error: ${error.message}`,
+          error.response.data.errors
+        );
+      } else {
+        reportStatus(`Encountered an error: ${error.message}`);
+      }
     });
 }
 
@@ -62,4 +120,5 @@ $(document).ready(() => {
     getTripDetails(id);
   });
 
+  $('#trip-form').submit(reserveTrip);
 });
