8690ac426e0a207c843251d17e1f5657efbe2643
marikoja <Marikoanderson@gmail.com>
2018-05-23 17:59:49 -0700

    refactors li appending, adds hidden and remove class tags for main content view, adds form for making reservation

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const reportStatus = (message) => {
 const loadTrips = () => {
   reportStatus('Loading Trips! Thank you for waiting...');
 
+  $('.main-display').removeClass('hidden');
+
   const tripList = $('#trip-list');
   tripList.empty();
 
@@ -15,9 +17,16 @@ const loadTrips = () => {
   axios.get(URL)
     // success vv
     .then((response) => {
+      console.log(response);
+      console.log('list item');
       response.data.forEach((trip) => {
-        console.log(response);
-        tripList.append(`<li>${trip.name}</li>`);
+        let item = $(`<li>${trip.name}</li>`);
+        item.click(function() {
+          getTripData(trip.id);
+          $('#trip-details').removeClass('hidden');
+          $('.reservations-form').removeClass('hidden');
+        });
+        tripList.append(item);
       });
     reportStatus('Trips Loaded');
     })
@@ -26,9 +35,58 @@ const loadTrips = () => {
       console.log(error);
       reportStatus(`Error: ${error.message}`);
     });
-
-
 }
+
+const getTripData = (trip_id) => {
+  const tripDetails = $('#trip-details');
+  tripDetails.empty();
+  let tripURL =  'https://ada-backtrek-api.herokuapp.com/trips/' + trip_id;
+  axios.get(tripURL)
+  .then((response) => {
+    console.log(response);
+    reportStatus(`Trip ${response.data.name} Loaded`);
+
+    tripDetails.append(`
+      <li>Trip: ${response.data.name}</li>
+      <li> Travel Category: ${response.data.category}</li>
+      <li> Continent: ${response.data.continent}</li>
+      <li> Cost: ${response.data.cost}</li>
+      <li> Trip Length: ${response.data.weeks} weeks</li>
+      `)
+      // <li>${response.data.about}</li>
+    })
+    .catch((error) => {
+      console.log(error);
+      reportStatus(`Error: ${error.message}`);
+  })
+}
+
+// RESERVATION FORM
+const reservationFormFields = ['name', 'age', 'email']
+
+const inputField = name => $(`#reservation-form input[name="${name}"]`);
+
+const readFormData = () => {
+  const getInput = name => {
+    const input = inputField(name).val();
+    return input ? input : undefined;
+  };
+
+  const formData = {};
+  reservationFromFields.forEach((field) => {
+    formData[field] = getInput(field);
+  });
+
+  return formData;
+};
+
+const clearForm = () => {
+  reservationFromFields.forEach((field) => {
+    inputField(field).val('');
+  });
+}
+
+
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
