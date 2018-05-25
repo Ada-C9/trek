3388b804f35bd30146c178d6604a5d00f5ed6895
emilcecarlisa <emilcecarlisa@gmail.com>
2018-05-24 22:17:05 -0700

    Post working

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const allTripsUrl = " https://ada-backtrek-api.herokuapp.com/trips";
 
 const singleTripUrl = "https://ada-backtrek-api.herokuapp.com/trips/";
 
-const reserveUrl = "https://ada-backtrek-api.herokuapp.com/trips/1/reservations"
+const reserveUrl = "https://ada-backtrek-api.herokuapp.com/trips/1/reservations";
+
 const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
 const reportError = (message, errors) => {
-  let content = `<p>${message}</p>`;
-  content += '<ul>';
-}
+  let content = `<p>${message}</p>`
+  content += "<ul>";
+  for (const field in errors) {
+    for (const problem of errors[field]) {
+      content += `<li>${field}: ${problem}</li>`;
+    }
+  }
+  content += "</ul>";
+  reportStatus(content);
+};
 
 const loadTrips = function loadTrips() {
   let tripList = $('.trips');
@@ -29,7 +37,7 @@ const loadTrips = function loadTrips() {
     if (error.response.data && error.response.data.errors) {
       // User our new helper method
       reportError(
-        `Encountered an error: ${error.message}`,
+        `Something's wrong: ${error.message}`,
         error.response.data.errors
       );
     } else {
@@ -41,6 +49,10 @@ const loadTrips = function loadTrips() {
 const getTrip = function getTrip(event) {
   event.preventDefault();
 
+  let display = $('#new-reserve');
+  display.show();
+
+
   axios.get(singleTripUrl + $(this).data("id"))
   .then((response) => {
     $('#details').empty().append(`<h2>${response.data.name}</h2><h3>${response.data.continent}</h3><p>${response.data.about}</p><p>Category of trip: ${response.data.category}</p><p>Duration of trip (in weeks): ${response.data.weeks}</p><p>$${response.data.cost}</p>`);
@@ -49,14 +61,10 @@ const getTrip = function getTrip(event) {
 }
 
 const FORM_FIELDS = ['name', 'email'];
-// this looks like a function, but does the var(?) name below have to match the name when you call it below --like this inputField(name)
 const inputField = name => $(`#trip-form input[name="${name}"]`);
 
-// Why is the function expression left without a name on the right side of the equal sign?
 const readFormData = () => {
-// Is getInput the variable that holds an anonymous function called name, which takes no arguments? Why no empty parens?
   const getInput = name => {
-// input holds the resulting value from calling inputField function
     const input = inputField(name).val();
     return input ? input : undefined;
   };
@@ -76,32 +84,34 @@ const clearForm = () => {
 
 const reserveTrip = (event) => {
   event.preventDefault();
-//
-  console.log(`THIS IS AN EVENT `);
 
-  let display = $('#new-reserve');
-  display.show();
+  console.log(`THIS IS AN EVENT`);
 
   const reserveData = readFormData()
   console.log(reserveData);
 
   axios.post(reserveUrl, reserveData)
     .then((response) => {
+      console.log(`SUCCESS: ${$(response.data.id)}`);
       reportStatus(`Successfully added a reservation with ID ${response.data.id}!`);
       clearForm();
     })
-// //     .catch(() => {
-// //
-//     // })
+    .catch((error) => {
+        console.log(`FAIL!: ${response.data.id}`);
+          console.log(error.response);
+          if (error.response.data && error.response.data.errors) {
+            reportError(
+              `Encountered an error: ${error.message}`,
+              error.response.data.errors
+            );
+          } else {
+            reportStatus(`Something must be wrong: ${error.message}`);
+          }
+        });
 };
 
 $(document).ready(() => {
   $('#load').click(loadTrips);
-  // figure out how to put multiple click actions on line below
   $('.trips').on('click', '.trip', getTrip);
-  $('.trips').on('click', '.trip', reserveTrip);
-
-  // $('#new-reserve').on('click', '.trip', reserveTrip);
   $('#trip-form').submit(reserveTrip);
-
 });
