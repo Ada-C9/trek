6639d837c5185d8dfb1bb44c6c557f52b4305d66
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-05-27 20:06:22 -0700

    Fixed cost to 2 decimals, some styling and image added

--- trek.js
+++ trek.js
@@ -28,82 +28,82 @@ const loadTrips = () => {
 // LOAD ONE TRIP
 
 const loadTrip = (id) => {
-  const tripInfo = $('#trip-info');
-  tripInfo.empty();
-  const reservationForm = $('#reservation-form');
-  reservationForm.empty();
+        const tripInfo = $('#trip-info');
+        tripInfo.empty();
+        const reservationForm = $('#reservation-form');
+        reservationForm.empty();
 
-  reportStatus('Loading trip info! Please wait...');
-  $('#details').show();
+        reportStatus('Loading trip info! Please wait...');
+        $('#details').show();
 
   // GET TRIP DETAILS FROM API
 
-  axios.get(URL + `/${id}`)
-  .then((response) => {
+        axios.get(URL + `/${id}`)
+          .then((response) => {
     // console.log(response);
-    tripInfo.append(`<tr><td><strong>Name: </strong>     ${response.data.name}</td></tr>
+          tripInfo.append(`<tr><td><strong>Name: </strong>     ${response.data.name}</td></tr>
       <tr><td><strong>Trip ID: </strong>${response.data.id}</td></tr>
       <tr><td><strong>Continent: </strong>${response.data.continent}</td></tr>
       <tr><td><strong>Category: </strong>${response.data.category}</td></tr>
       <tr><td><strong>Weeks: </strong>${response.data.weeks}</td></tr>
-      <tr><td><strong>Cost: </strong>${response.data.cost}</td></tr>
+      <tr><td><strong>Cost: </strong>${response.data.cost.toFixed(2)}</td></tr>
       <tr><td><strong>About: </strong></br>${response.data.about}</td></tr>`);
-      reportStatus('Trip Info Loaded!');
-      reservationForm.append(`<div >
+          reportStatus('Trip Info Loaded!');
+          reservationForm.append(`<div >
         <label for="name">Trip:${response.data.name}</label>
-      </div>`);
-      reservationForm.append(`<div>
+        </div>`);
+          reservationForm.append(`<div>
           <label for="name">Name: </label>
           <input type="text" name="user-name" />
-        </div>
-        <div>
+          </div>
+          <div>
           <label for="email">Email: </label>
           <input type="text" name="email" />
-        </div>
-        <div class="submit-container">
+          </div>
+          <div class="submit-container">
           <input type="submit" name="add-reservation" value="Reserve" class="reserve" id="reserve${response.data.id}"/>
-        </div>`);
+          </div>`);
 
-    })
-    .catch((error) => {
-      reportStatus('Error: ${error.message}');
-    });
-};
+        })
+        .catch((error) => {
+          reportStatus('Error: ${error.message}');
+        });
+      };
 
+      //RESERVE TRIP
 
-  //RESERVE TRIP
+const reserveTrip = (id) => {
+        reportStatus('');
+        reportStatus('Reserving The Trip...');
 
-  const reserveTrip = (id) => {
-  reportStatus('');
-  reportStatus('Reserving The Trip...');
+        let userData = {
+          'name': $('input[name="user-name"]').val(),
+          'email': $('input[name="email"]').val(),
+        };
 
-  let userData = {
-    'name': $('input[name="user-name"]').val(),
-    'email': $('input[name="email"]').val()
-  }
+        axios.post(URL + `/${id}/reservations`, userData)
+        .then((response) => {
+          reportStatus(`Successfully reserved this trip with the name ${response.data.name}`);
+        })
+        .catch((error) => {
+          reportStatus(`Encountered an error: ${error.message}`);
+        });
 
-  axios.post(URL + `/${id}/reservations`, userData)
-    .then((response) => {
-      reportStatus(`Successfully reserved this trip with the name ${response.data.name}`);
-      })
-    .catch((error) => {
-      reportStatus(`Encountered an error: ${error.message}`);
-      });
+        $('input[name="user-name"]').val('');
+        $('input[name="email"]').val('');
+      };
 
-  $('input[name="user-name"]').val('');
-  $('input[name="email"]').val('');
-}
-
-// ACTION
+      // ACTION
 $(document).ready(() => {
-    $('#load').click(loadTrips);
-    $('#tbody').on('click', 'tr', function () {
-      let id = $(this).attr('id');
-      loadTrip(id);
-    });
+        $('#load').click(loadTrips);
+        $('#tbody').on('click', 'tr', function () {
+          let id = $(this).attr('id');
+          loadTrip(id);
+        });
 
-    $('#reservation-form').on('click', '.reserve', function () {
-      let id = $(this).attr('id').substr(7);
-      reserveTrip(id);
-    });
-  });
+        $('#reservation-form').on('click', '.reserve', function () {
+          let id = $(this).attr('id').substr(7);
+          reserveTrip(id);
+          $(document).foundation();
+        });
+      });
