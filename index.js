1d012c66017dc5b2d3908df1d5fbe9d4ec641fe0
SelamawitA <Selamawit.ainalem@gmail.com>
2018-05-28 18:34:35 -0700

    fixed few bugs, could not get trip book to toggle with error messages, cleaned up lines for index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-//click on reservation
-//form appears - input name (on click event - prepopulated)
-
-//Use a closures design where the outside function is the
-//click function of a single element in the trips list.
-//the inside element is going to be the inner function.
-
 const URL = `https://ada-backtrek-api.herokuapp.com/trips`
+const postURL = `https://ada-backtrek-api.herokuapp.com/trips`
+
 
 let singleTripURL = function(tripID){
   let singleURL = ``
@@ -17,7 +12,6 @@ let reserveAtrip = function(tripID){
   return singleURL+= `https://ada-backtrek-api.herokuapp.com/trips/${tripID}/reservations`
 }
 
-const postURL = `https://ada-backtrek-api.herokuapp.com/trips`
 
 const reportStatus = (message) =>{
   $(`#status-message`).html(message);
@@ -35,8 +29,6 @@ const reportError = (message, errors) => {
   reportStatus(content);
 };
 
-
-
 const loadTripdetails = (aTrip)=>{
   const tripDetails = $(`#trip-details`);
   const divTrips = $('#div-for-trips');
@@ -46,33 +38,33 @@ const loadTripdetails = (aTrip)=>{
   reportStatus('Searching for trip details...')
 
   axios.get(singleTripURL(aTrip.target.id))
-    .then((response) => {
-        tripDetails.append(`<li>${response.data.name}</li>`)
-        tripDetails.append(`<li>${response.data.continent}</li>`)
-        tripDetails.append(`<li>${response.data.category}</li>`)
-        tripDetails.append(`<li>${response.data.weeks}</li>`)
-        tripDetails.append(`<li>${response.data.cost}</li>`)
-        tripDetails.append(`<li>${response.data.about}</li>`)
-        tripDetails.append(`<button id="make-res">Book this trip!</button>`)
-        divTrips.append(`<input type="hidden" name="location-id" value="${response.data.id}"/>`)
-        divTrips.append('<label for="trip">Trip</label>')
-        divTrips.append(`<input type="trip" name="trip" id="trip-input" value=${response.data.name}/>`)
-      reportStatus('Trip details found!')
-    })
+  .then((response) => {
+    tripDetails.append(`<li>Trip Location: ${response.data.name}</li>`)
+    tripDetails.append(`<li>Continent: ${response.data.continent}</li>`)
+    tripDetails.append(`<li>Category: ${response.data.category}</li>`)
+    tripDetails.append(`<li>Weeks: ${response.data.weeks}</li>`)
+    tripDetails.append(`<li>Cost: $${response.data.cost}</li>`)
+    tripDetails.append(`<li>Details: ${response.data.about}</li>`)
+    tripDetails.append(`<button class="button" id="make-res">Book this trip!</button>`)
+    divTrips.append(`<input type="hidden" name="location-id" value="${response.data.id}"/>`)
+    divTrips.append('<label for="trip" >Trip</label>')
+    divTrips.append(`<input type="trip" name="trip" id="trip-input" value="${response.data.name}"/>`)
+    reportStatus('Trip details found!')
+  })
   .catch((error) =>{
-      console.log(error)
-      reportStatus(`Error: ${error.message}`)
-    })
+    console.log(error)
+    reportStatus(`Error: ${error.message}`)
+  })
 }
 
 const loadTrips = ()=>{
   //create reference to all-trips element
-  const tripList = $(`#all-trips`);
+  let tripList = $(`#all-trips`);
   //empty list before call to API
   tripList.empty();
   //display pending status to user
   reportStatus('Loading trips....')
-axios.get(URL)
+  axios.get(URL)
   .then((response) => {
     response.data.forEach((trip) => {
       tripList.append(`<li id="${trip.id}">${trip.name}</li>`)
@@ -89,15 +81,14 @@ const createTrip = (event) =>{
   event.preventDefault();
 
   const newTripinfo = {
-      name: $(`input[name="name-create"]`).val(),
-      continent: $(`input[name="continent"]`).val(),
-      about: $(`input[name="about"]`).val(),
-      category: $(`input[name="category"]`).val(),
-      weeks: $(`input[name="weeks"]`).val(),
-      cost: $(`input[name="cost"]`).val()
-
+    name: $(`input[name="name-create"]`).val(),
+    continent: $(`input[name="continent"]`).val(),
+    about: $(`input[name="about"]`).val(),
+    category: $(`input[name="category"]`).val(),
+    weeks: $(`input[name="weeks"]`).val(),
+    cost: $(`input[name="cost"]`).val()
   }
-console.log(newTripinfo)
+
   reportStatus(`Attempting to create your new trip!`)
   $(`input[name="name-create"]`).val('')
   $(`input[name="continent"]`).val('')
@@ -111,10 +102,12 @@ console.log(newTripinfo)
     reportStatus('Your trip has been created')
   })
   .catch((error) =>{
-    console.log(error)
-    reportStatus(`Error: ${error.message}`)
+    reportStatus('fail')
+    if( error.message && error.response.data.errors){
+      reportError(error.message,error.response.data.errors)
+    }else
+    reportError(`encounted error: ${error.message}`)
   })
-//end of method
 };
 
 
@@ -127,84 +120,55 @@ const bookTrip = (event) =>{
     tripID:$(`input[name="location-id"]`).val()
   }
 
-  // console.log(tripInfo)
-
   reportStatus(`Attempting to make your reservation`)
   $(`input[name="name"]`).val('')
   $(`input[name="email"]`).val('')
   $(`input[name="location-id"]`).val('')
 
-axios.post(reserveAtrip(tripInfo.tripID),tripInfo)
+  axios.post(reserveAtrip(tripInfo.tripID),tripInfo)
   .then((response) => {
     reportStatus('You trip has been booked')
   })
   .catch((error) => {
     reportStatus('fail')
     if( error.message && error.response.data.errors){
-    reportError(error.message,error.response.data.errors)
-  }else
+      let v = error.response.data.errors
+      console.log(v)
+      reportError(error.message,error.response.data.errors)
+    }else
     reportError(`encounted error: ${error.message}`)
- })
-//end of method
+  })
 };
 
 
-
-
-/// DOCUMENT . READY ///
-// / Original Code //
-
 $(document).ready(() => {
-  //all trips
-   $(`#load`).click(loadTrips);
-   //single trip
-   $(document).on("click","#all-trips",function(event){
-     loadTripdetails(event);
-   });
+  $(`#load`).click(loadTrips);
+  $(document).on("click","#all-trips",function(event){
+    loadTripdetails(event);
+  });
 
-   $(document).on("click","#make-res",function(){
-      $("#book-trip-form").toggle();
-   });
+  $(document).on("click","#make-res",function(){
+    $("#book-trip-form").toggle("slow");
+  });
 
-   $(`#book-trip-form`).submit(bookTrip);
+  $(`#book-trip-form`).submit(bookTrip);
 
-   $(document).on("click","#submit-button",function(){
-     $('#book-trip-form').toggle()
-   })
+  $(document).on("click","#submit-button",function(event){
+    $(`#submit-button`).toggle();
+    $(`#book-trip-form`).append(`<button id="button-success" class="button success">Trip Booked!</button>`);
+  });
 
-   $(`#create-trip`).submit(createTrip);
-//post a reservation
+  $(document).on("click","#create-trip-button",function(){
+    $(`#create-trip`).toggle("slow");
+  })
 
+  $(`#create-trip`).submit(createTrip);
+  $(document).on("click","#submit-trip",function(){
+    $('#create-trip').toggle("slow")
+  })
 
+  $(document).on("click","#all-trips",function(){
+    $(`#button-success`).remove();
+    $(`#submit-button`).show();
+  })
 });
-
-
-// Come back to closures
-
-// $(document).ready(() => {
-//   //all trips
-//    $(`#load`).click(loadTrips);
-//    //single trip
-//
-//    const clickOntrip = function(){
-//      $(document).on("click","all-trips",function(event){
-//        loadTripdetails(event);
-//      }
-//
-//       )
-//    }
-//    $(document).on("click","#all-trips", function(event) {
-//      // loadTripdetails(event);
-//      //  console.log($(`#name`).val());
-//      // here you're attempting to call an item before it has been created by javascript.
-//      $(document).on("click","#make-reservation", function() {
-//        // $("#trip-input").val(event.result)
-//        // console.log(singleTripevent)
-//        $("#trip-form").toggle("slow");
-//        // $("#trip-input").val(`${event.result.name}`)
-//      })
-//    });
-
-
-//
-// });
